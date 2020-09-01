a582ce548c54b5b34149c28c7431022c68651790
shen4brains <kshen@research.baycrest.org>
2020-09-01 17:26:27 -0400

    fix bedpostx and probtrackx

--- bb_diffusion_pipeline/bb_pipeline_diff.py
+++ bb_diffusion_pipeline/bb_pipeline_diff.py
@@ -94,18 +94,18 @@ def bb_pipeline_diff(subject, jobHold, fileConfiguration):
         + baseDir
         + "/dMRI/dMRI/dti",
     )
-    #jobTBSS = LT.runCommand(
-        #logger,
-        ##'${FSLDIR}/bin/fsl_sub -T 240 -N "bb_tbss_'
-        #'${FSLDIR}/bin/fsl_sub -q bigmem_16.q -N "bb_tbss_'
-        #+ subname
-        #+ '" -j '
-        #+ jobDTIFIT
-        #+ "  -l "
-        #+ logDir
-        #+ " $BB_BIN_DIR/bb_diffusion_pipeline/bb_tbss/bb_tbss_general "
-        #+ subject,
-    #)
+    jobTBSS = LT.runCommand(
+        logger,
+        #'${FSLDIR}/bin/fsl_sub -T 240 -N "bb_tbss_'
+        '${FSLDIR}/bin/fsl_sub -q bigmem_16.q -N "bb_tbss_'
+        + subname
+        + '" -j '
+        + jobDTIFIT
+        + "  -l "
+        + logDir
+        + " $BB_BIN_DIR/bb_diffusion_pipeline/bb_tbss/bb_tbss_general "
+        + subject,
+    )
     #jobNODDI = LT.runCommand(
         #logger,
         ##'${FSLDIR}/bin/fsl_sub -T 100 -N "bb_NODDI_'
@@ -138,12 +138,14 @@ def bb_pipeline_diff(subject, jobHold, fileConfiguration):
         + subname
         + '" -j '
         + jobPREBEDPOSTX
-        + "  -q $FSLGECUDAQ -l "
+        #+ "  -q $FSLGECUDAQ -l "
+        + "  -l "
         + logDir
         + " $BB_BIN_DIR/bb_diffusion_pipeline/bb_bedpostx/bb_bedpostx_gpu "
         + baseDir
         + "/dMRI",
     )
+    ##### bb_post_bedpostx_gpu not necessary if using bedpostx package rather than xfibres (gpu) #####
     #jobPOSTBEDPOSTX = LT.runCommand(
         #logger,
         ##'${FSLDIR}/bin/fsl_sub -T 15  -N "bb_post_bedpostx_gpu_'
@@ -157,6 +159,7 @@ def bb_pipeline_diff(subject, jobHold, fileConfiguration):
         #+ baseDir
         #+ "/dMRI/dMRI",
     #)
+    #### running our own tractography algorithms so turning AutoPtx option off
     #jobAUTOPTX = LT.runCommand(
         #logger,
         #"$BB_BIN_DIR/bb_diffusion_pipeline/bb_autoPtx/bb_autoPtx "
@@ -166,6 +169,7 @@ def bb_pipeline_diff(subject, jobHold, fileConfiguration):
         #+ ","
         #+ jobTBSS,
     #)
+    
     jobPREPROBTRACKX = LT.runCommand(
         logger,
         '${FSLDIR}/bin/fsl_sub -q all.q -N "bb_pre_probtrackx_'
