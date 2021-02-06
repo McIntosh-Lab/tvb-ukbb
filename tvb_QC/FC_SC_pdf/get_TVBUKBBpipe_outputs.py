2ecd0589b4eea28821a4925b61d97c56f3dbee22
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2021-02-06 01:07:52 -0500

    changed SC_FC_pdf script to include both fMRI_0 .icaa and fMRI_1.ica

--- tvb_QC/FC_SC_pdf/get_TVBUKBBpipe_outputs.py
+++ tvb_QC/FC_SC_pdf/get_TVBUKBBpipe_outputs.py
@@ -33,10 +33,14 @@ def get_TVBUKBBpipe_outputs(subj_list, out_dir):
         SC=np.divide(SC_abs,waytotal)
 
         tract_lengths=np.loadtxt(subj + '/dMRI/probtrackx/fdt_network_matrix_lengths')
-        FC=np.loadtxt(subj + '/fMRI/rfMRI_1.ica/fc.txt')   #rfMRI.ica does not exist in the sample data... replaced with rfMRI_1.ica for now
-        norm_ts=zscore(np.loadtxt(subj + '/fMRI/rfMRI_1.ica/ts.txt'));   #rfMRI.ica does not exist in the sample data... replaced with rfMRI_1.ica for now
+        
+        FC_0=np.loadtxt(subj + '/fMRI/_rfMRI_0.ica/fc.txt')   
+        norm_ts_0=zscore(np.loadtxt(subj + '/fMRI/_rfMRI_0.ica/ts.txt'));   #rfMRI.ica does not exist in the sample data... replaced with rfMRI_1.ica for now
 
-        f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(50, 50))
+        FC_1=np.loadtxt(subj + '/fMRI/rfMRI_1.ica/fc.txt')   
+        norm_ts_1=zscore(np.loadtxt(subj + '/fMRI/rfMRI_1.ica/ts.txt'));   #rfMRI.ica does not exist in the sample data... replaced with rfMRI_1.ica for now
+
+        f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(50, 50))
         
         #division by 0 occurs in np.log10(SC) if 0 values aren't turned into small values
         for i in range(SC.shape[0]):
@@ -53,14 +57,23 @@ def get_TVBUKBBpipe_outputs(subj_list, out_dir):
         im2= ax2.imshow(tract_lengths)
         f.colorbar(im2, ax=ax2)
         
-        ax3.set_title('FC')
-        im3= ax3.imshow(FC)
+        ax3.set_title('FC_0')
+        im3= ax3.imshow(FC_0)
         f.colorbar(im3, ax=ax3)
         
         ax4.set_xlabel('volume')
         ax4.set_ylabel('ROI')
-        ax4.set_title('ROI timeseries carpet plot')
-        im4= ax4.imshow(norm_ts.transpose(), cmap='gray', aspect = 'auto')
+        ax4.set_title('TS_0 ROI timeseries carpet plot')
+        im4= ax4.imshow(norm_ts_0.transpose(), cmap='gray', aspect = 'auto')
+        
+        ax5.set_title('FC_1')
+        im5= ax5.imshow(FC_1)
+        f.colorbar(im5, ax=ax5)
+        
+        ax6.set_xlabel('volume')
+        ax6.set_ylabel('ROI')
+        ax6.set_title('TS_1 ROI timeseries carpet plot')
+        im6= ax6.imshow(norm_ts_1.transpose(), cmap='gray', aspect = 'auto')
        
         saveNm=out_dir + '/' +subjName + '_SCFC.pdf'
         f.savefig(saveNm)
@@ -70,9 +83,9 @@ def get_TVBUKBBpipe_outputs(subj_list, out_dir):
 
 
 if __name__ == "__main__":
-    try:
-        get_TVBUKBBpipe_outputs(sys.argv[1],sys.argv[2])
-    except:
-        print("ERROR. Usage: python get_TVBUKBBpipe_outputs.py subj_list out_dir\nsubj_list: a .txt file of subject directories with the full path specified\nout_dir: directory where each subject's plots will be saved")
+    #try:
+    get_TVBUKBBpipe_outputs(sys.argv[1],sys.argv[2])
+    #except:
+    #    print("ERROR. Usage: python get_TVBUKBBpipe_outputs.py subj_list out_dir\nsubj_list: a .txt file of subject directories with the full path specified\nout_dir: directory where each subject's plots will be saved")
         
     #TODO more error handling here and in function def to deal with invalid files and paths
