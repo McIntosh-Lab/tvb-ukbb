5ca54d1a1f407f51f9b095a81a46bf11af19e16b
shen4brains <kshen@research.baycrest.org>
2021-02-09 12:08:16 -0500

    initial changes to customize pipe for PPMI

--- bb_pipeline_tools/bb_file_manager.py
+++ bb_pipeline_tools/bb_file_manager.py
@@ -688,7 +688,7 @@ def bb_file_manager(subject):
     patterns_actions = [
         [["*.[^log]"], capitalize_and_clean],
         [["dicom", "DICOM"], move_to, "delete/"],
-        [["*T1*.nii.gz"], manage_struct, "T1"],
+        [["*T1*.nii.gz","*MPRAGE*.nii.gz"], manage_struct, "T1"],
         [["T2*FLAIR*.nii.gz", "*FLAIR*.nii.gz"], manage_struct, "T2"],
         [
             [
@@ -697,6 +697,7 @@ def bb_file_manager(subject):
                 "*TASK*REST*.nii.gz",
                 "*task*rest*.nii.gz",
                 "*epi_rest*.nii.gz",
+                "*RESTING_STATE*.nii.gz",
             ],
             manage_fMRI,
             "rfMRI",
@@ -713,8 +714,7 @@ def bb_file_manager(subject):
             "tfMRI",
         ],
         [["SWI*nii.gz"], manage_SWI],
-        # [["DIFF_*", "MB3_*", "*dwi*.nii.gz", "*DWI*.nii.gz"], manage_DWI],
-        [["DIFF_*", "MB3_*", "*dwi*.*", "*DWI*.*"], manage_DWI],
+        [["DIFF_*", "MB3_*", "*dwi*.*", "*DWI*.*","*DTI_gated.*", manage_DWI],
         [["SWI*.*"], move_to, "SWI/unclassified/"],
         [["*.[^log]"], move_to, "unclassified/"],
     ]
