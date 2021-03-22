7a5b077acb67d56da547223b785cf395bb994306
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2021-03-22 05:14:00 -0400

    removing image_gen_links.js if exists before script runs. created togglesMELODIC.js, MELODIC tab to all pages, MELODIC_html_gen.sh. integrating MELODIC html gen to html gen.

---
+++ tvb_bb_QC/MELODIC_html_gen.sh
@@ -0,0 +1,360 @@
+#!/bin/bash
+
+
+dirSubject=$1
+
+sub=$2
+sub_upper=${sub}
+
+
+mkdir -p $dirSubject"/QC/html"
+
+
+MELODIC_html=$dirSubject"/QC/html/MELODIC.html"
+
+rm $MELODIC_html
+
+if [ ! -e $MELODIC_html ]; then
+  
+cat > $MELODIC_html << EOF
+
+
+<!DOCTYPE html>
+<html lang="en">
+<title>MELODIC IMAGE REPORT</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/w3.css">
+<script src="./togglesMELODIC.js" type="text/javascript" charset="utf-8"></script>
+<script src="./sidebartoggles.js" type="text/javascript" charset="utf-8"></script>
+
+<body class="w3-main" style="background-color:black; " onload="updateTitle();updateImage();updateTitle();">
+<!--document.getElementById('image_link').innerHTML = document.getElementById('ex_a').src;-->
+
+
+<!-- Header -->
+<header class="w3-display-container  w3-center" style="max-height:1000px; ">
+  <img class="w3-image" src="images/1.jpg" alt="Me" width=100% style="max-height:1000px;object-fit: cover;">
+   
+    <div class="w3-display-middle w3-padding-large w3-border w3-wide w3-text-light-grey w3-center" style="background-color:rgba(0, 0, 0, 0.75);">
+
+      <h1 class="w3-hide-medium w3-hide-small w3-xxxlarge">MELODIC IMAGE REPORT</h1>
+      <h5 class="w3-hide-large" style="white-space:nowrap">MELODIC IMAGE REPORT</h5>
+      
+      <h3 class="w3-hide-medium w3-hide-small">$sub_upper</h3>
+      
+    </div>
+ 
+
+  <!-- Sidebar -->
+
+  <div id="mySidebar" class="sidebar" style="width: 0; margin-left: 0; font-size: 12px">
+    <a href="file:notes/" class="w3-center">open notes.txt file in local system to make changes</a>
+    <br>
+    <iframe src="notes/notes.txt" style="background: #FFFFFF; height: 90%"></iframe>
+
+  </div>
+
+  <!-- Sidebar button -->
+   <div id="main" align="left" >
+    <button id="togglebutton" class="openbtn sticky" onclick="toggNav()">NOTES</button>  
+  </div>
+
+
+
+
+  <!-- Navbar (placed at the bottom of the header image) -->
+  <div class="w3-bar w3-light-grey w3-round w3-display-bottommiddle w3-hide-small w3-hide-medium" style="bottom:65px">
+    <a href="report.html" class="w3-bar-item w3-button">Home</a>
+    <a href="anat.html" class="w3-bar-item w3-button">Anatomical</a>
+    <a href="fMRI.html" class="w3-bar-item w3-button">fMRI</a>
+    <a href="MELODIC.html" class="w3-bar-item w3-button">MELODIC</a>
+    <a href="DTI.html" class="w3-bar-item w3-button">DTI</a>
+    <a href="SCFC.html" class="w3-bar-item w3-button">SC/FC</a>
+  </div>
+</header>
+
+<!-- Navbar on small screens -->
+<div class="w3-center w3-black w3-padding-16 w3-hide-large ">
+<div class="w3-bar w3-light-grey w3-round ">
+  <a href="report.html" class="w3-bar-item w3-button">Home</a>
+    <a href="anat.html" class="w3-bar-item w3-button">Anatomical</a>
+    <a href="fMRI.html" class="w3-bar-item w3-button">fMRI</a>
+    <a href="MELODIC.html" class="w3-bar-item w3-button">MELODIC</a>
+    <a href="DTI.html" class="w3-bar-item w3-button">DTI</a>
+    <a href="SCFC.html" class="w3-bar-item w3-button">SC/FC</a>
+</div>
+</div>
+
+
+<!-- Page content -->
+<div class="w3-content w3-padding-large w3-margin-top" id="portfolio" style="color:white; text-align: center; height: 900px">
+
+
+  <!-- Images (Portfolio) -->
+
+
+  <br>______<br><br>
+  <h1> $sub_upper <div id="analysis_title_0" style="display: inline;"></div> 
+  <br>
+</h1>
+  <br>
+
+
+
+
+
+<!-------------- OPTIONS -------------->
+
+<!-- ICAs -->
+  <label for="ICA" style="white-space:nowrap;">ICA <i>(q/e)</i>:
+  <select name="ICA" id="ICA" onchange="updateTitle();updateImage();" onkeydown="IgnoreAlpha(event);">
+    
+
+EOF
+
+fi
+
+
+
+
+
+
+
+array=()
+while IFS=  read -r -d $'\0'; do
+    array+=("$REPLY")
+done < <(find $dirSubject/fMRI -maxdepth 1 -type d -name "*.ica" -print0)
+
+
+
+#for each .ica file
+for t in ${array[@]}; do
+  rfMRI_ver=`basename $t`
+cat <<EOF >> $MELODIC_html
+
+<option value="$rfMRI_ver" id="$rfMRI_ver">$rfMRI_ver</option>
+
+EOF
+done
+
+
+#printing inbetween filler
+
+cat <<EOF >> $MELODIC_html
+
+  </select></label>
+  &nbsp&nbsp&nbsp&nbsp
+
+
+EOF
+
+
+
+#printing components
+for t in ${array[@]}; do
+  rfMRI_ver=`basename $t`
+  #deal with multiple fix4melview
+  file=$(find $t -maxdepth 1 -name "fix4melview_*.txt" -print0)
+
+
+    num=`tail -n2 $file| head -n1`
+  noise=`tail -n1 $file| head -n1`
+    
+    num="${num%%,*}"
+
+  noise=${noise#"["}
+  noise=${noise%"]"}
+  IFS=', ' read -r -a noise_array <<< "$noise"
+
+
+cat <<EOF >> $MELODIC_html
+
+<!-- Component -->
+
+  <label for="$rfMRI_ver" id="${rfMRI_ver}_label" style="white-space:nowrap; display: none;">Component <i>(a/d)</i>:
+  <select name="$rfMRI_ver" id="${rfMRI_ver}_select" onchange="updateTitle();updateImage();" onkeydown="IgnoreAlpha(event);">
+
+    <optgroup label="Signal">
+EOF
+
+
+for ((n=1;n<=$num;n++)); do
+  if [[ " ${noise_array[@]} " =~ " $n " ]]; then
+      SIG_or_NOISE="NOISE"
+  else
+cat <<EOF >> $MELODIC_html
+      <option value="$n" id="$n">SIGNAL: IC_$n</option>
+EOF
+  fi
+done
+
+
+cat <<EOF >> $MELODIC_html
+    </optgroup>
+    <optgroup label="Noise">
+EOF
+
+
+
+for ((n=1;n<=$num;n++)); do
+  if [[ " ${noise_array[@]} " =~ " $n " ]]; then
+cat <<EOF >> $MELODIC_html
+      <option value="$n" id="$n">NOISE: IC_$n</option>
+EOF
+  else
+    SIG_or_NOISE="SIGNAL"
+  fi
+done
+
+
+
+cat <<EOF >> $MELODIC_html
+    </optgroup>
+  </select></label>
+
+
+EOF
+done
+
+
+
+cat <<EOF >> $MELODIC_html
+
+<br>
+<br>
+  <!-- Analyses -->
+  <label for="Analysis" style="white-space:nowrap;">Analysis <i>(z/c)</i>:
+  <select name="Analysis" id="Analysis" onchange="updateTitle();updateImage();" onkeydown="IgnoreAlpha(event);">
+    
+      <option value="ic" id="ic">IC Map</option>
+      <option value="th" id="th">Thresholded IC Map</option>
+      <option value="mm" id="mm">Mixture Model Probability Map</option>
+      <option value="pt" id="pt">Powerspectrum of Timecourse (frequency)</option>
+      <option value="tc" id="tc">Timecourse (time)</option>
+      <option value="gg" id="gg">Gaussian/Gamma Mixture Model Fit</option>
+  </select></label>
+  &nbsp&nbsp&nbsp&nbsp
+
+
+<!-------------- OVERLAYS -------------->
+
+
+  <br> <br> 
+
+______<br><br>
+<div id="analysis_title_1" style="display: inline; font-size: 28px"></div>
+
+
+
+EOF
+
+
+
+
+for t in ${array[@]}; do
+  
+  folder=`basename $t`
+  echo $folder
+
+
+    file=$(find $t -maxdepth 1 -name "fix4melview_*.txt" -print0)
+
+
+    num=`tail -n2 $file| head -n1`
+    noise=`tail -n1 $file| head -n1`
+    
+    num="${num%%,*}"
+
+  noise=${noise#"["}
+  noise=${noise%"]"}
+  IFS=', ' read -r -a noise_array <<< "$noise"
+
+
+  rfMRI_ver=`basename $t`
+
+  report_dir=$rfMRI_ver/filtered_func_data.ica/report
+
+
+  home=$report_dir/00index.html
+  EV=$report_dir/EVplot.png
+
+  for ((n=1;n<=$num;n++)); do
+
+    SIG_or_NOISE=""
+
+    if [[ " ${noise_array[@]} " =~ " $n " ]]; then
+      SIG_or_NOISE="NOISE"
+      # whatever you want to do when array contains value
+    else
+      SIG_or_NOISE="SIGNAL"
+    fi
+
+      IC_html=$report_dir/IC_${n}.html
+    IC_MM_html=$report_dir/IC_${n}_MM.html
+
+    IC_MMfit_png=$report_dir/IC_${n}_MMfit.png
+    IC_prob_png=$report_dir/IC_${n}_prob.png
+    IC_thresh_png=$report_dir/IC_${n}_thresh.png
+    IC_png=$report_dir/IC_${n}.png
+
+    f_png=$report_dir/f${n}.png
+    t_png=$report_dir/t${n}.png
+
+
+
+cat <<EOF >> $MELODIC_html
+
+
+<div class="c_${n}_$rfMRI_ver" style="display: none;">
+<a href="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/00index.html">MEDLODIC ICA Home</a><br>
+&nbsp&nbsp<a href="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/IC_${n}.html">IC Report</a> &nbsp&nbsp-&nbsp&nbsp  
+<a href="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/IC_${n}_MM.html">IC MM report</a><br>
+
+      <br>
+<img id="${rfMRI_ver}_${n}_ic" src="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/IC_${n}.png" class="${n}" width="1000" >
+<img id="${rfMRI_ver}_${n}_th" src="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/IC_${n}_thresh.png" class="${n}" width="1000" >
+<img id="${rfMRI_ver}_${n}_mm" src="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/IC_${n}_prob.png" class="${n}" width="1000" >
+<img id="${rfMRI_ver}_${n}_pt" src="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/f${n}.png" class="${n}" width="1000" >
+<img id="${rfMRI_ver}_${n}_tc" src="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/t${n}.png" class="${n}" width="1000" >
+<img id="${rfMRI_ver}_${n}_gg" src="../../fMRI/$rfMRI_ver/filtered_func_data.ica/report/IC_${n}_MMfit.png" class="${n}" width="1000" >
+</div>
+
+
+EOF
+
+    #output=$ok$'\n'
+
+
+  done
+
+done
+
+
+
+
+cat <<EOF >> $MELODIC_html
+
+
+ <br> <br>
+ 
+
+
+<div style="text-align: left; font-size: 12px" >
+IMAGE 1 FILE:<br> <a id="image_link" href="../images/T1_extraction_masked/sub-CC520055_T1_extraction_masked_axial.png">N/A</a>
+<br><br><br>
+</div>
+<!-- End page content -->
+</div>
+
+</body>
+
+<script>
+ updateTitle();updateImage();
+</script>
+
+
+
+</html>
+
+EOF
---
+++ tvb_bb_QC/resources/togglesMELODIC.js
@@ -0,0 +1,199 @@
+
+  let view
+  let canvas
+
+
+document.addEventListener('keyup', logKey);
+
+
+function IgnoreAlpha(e) {
+  if (!e) {
+    e = window.event;
+  }
+  if (e.keyCode >= 65 && e.keyCode <= 90) // A to Z
+  {
+    e.returnValue = false;
+    e.cancel = true;
+  }
+}
+
+
+function logKey(e) {
+
+  var component_type=document.getElementById("ICA").options[document.getElementById("ICA").selectedIndex].text;
+
+  if(e.code=="KeyQ"){
+    var select = document.getElementById('ICA');
+    if(select.selectedIndex==0){
+      select.selectedIndex=select.length - 1
+    }
+    else{
+        select.selectedIndex--;
+    }
+    updateTitle();updateImage();update_links();
+  }
+
+
+  if(e.code=="KeyE"){
+    var select = document.getElementById('ICA');
+    if(select.selectedIndex==select.length - 1){
+      select.selectedIndex=0
+    }
+    else{
+      select.selectedIndex++;
+    }
+    updateTitle();updateImage();update_links();
+  }
+  if(e.code=="KeyA"){
+    var select = document.getElementsByName(component_type)[0];
+    if(select.selectedIndex==0){
+      select.selectedIndex=select.length - 1
+    }
+    else{
+        select.selectedIndex--;
+    }
+    updateTitle();updateImage();update_links();
+  }
+
+
+  if(e.code=="KeyD"){
+    var select = document.getElementsByName(component_type)[0];
+    if(select.selectedIndex==select.length - 1){
+      select.selectedIndex=0
+    }
+    else{
+      select.selectedIndex++;
+    }
+    updateTitle();updateImage();update_links();
+  }
+
+  if(e.code=="KeyZ"){
+    var select = document.getElementById('Analysis');
+    if(select.selectedIndex==0){
+      select.selectedIndex=select.length - 1
+    }
+    else{
+        select.selectedIndex--;
+    }
+    updateTitle();updateImage();update_links();
+  }
+
+
+  if(e.code=="KeyC"){
+    var select = document.getElementById('Analysis');
+    if(select.selectedIndex==select.length - 1){
+      select.selectedIndex=0
+    }
+    else{
+      select.selectedIndex++;
+    }
+    updateTitle();updateImage();update_links();
+  }
+
+}
+/*
+q zoom out
+w reset
+e zoom in
+a prev an
+s toggle overlay (if not hidden) 
+d next an
+z 1st ori or b=0 (if not hidden)  
+x 2ndr ori or b=1000  (if not hidden) 
+c 3rd ori or b=2000  (if not hidden) 
+
+*/
+
+  function initDraw() {
+    view.init()
+    view.draw()
+  }
+
+  function resetDraw() {
+    view.reset()
+    view.draw()
+  }
+
+  function reDraw() {
+    if(document.getElementById('zoom_keep').checked==true){
+      resetDraw()
+    }
+    else{
+      initDraw()
+    }
+  }
+
+
+  function start(image) {
+    canvas = document.getElementById('canvas')
+    let img = document.getElementById(image);
+    view = new Viewer(canvas, img)
+    view.draw()
+  }
+
+
+  function updateTitle(){
+    var color="red";
+    var component_type=document.getElementById("ICA").options[document.getElementById("ICA").selectedIndex].text;
+
+    for(var i=0;i<document.getElementById("ICA").length;i++){
+      var j = document.getElementById("ICA")[i].value;
+      document.getElementById(j+"_label").style.display = "none";
+    }
+    document.getElementById(component_type+"_label").style.display = "inline";
+
+
+
+    if (document.getElementsByName(component_type)[0].options[document.getElementsByName(component_type)[0].selectedIndex].text.toUpperCase().startsWith("SIGNAL") ){
+      color="green"
+    }
+    //document.getElementById("analysis_title").innerHTML = document.getElementById("Analysis").options[document.getElementById("Analysis").selectedIndex].text.toUpperCase();
+
+    document.getElementById("analysis_title_0").innerHTML = document.getElementById("ICA").options[document.getElementById("ICA").selectedIndex].text;
+
+    document.getElementById("analysis_title_1").innerHTML = "<div style='color: "+color+"'>"+document.getElementsByName(component_type)[0].options[document.getElementsByName(component_type)[0].selectedIndex].text.toUpperCase()+"</div>";
+
+  }
+
+  function updateImage(){
+    var component_type=document.getElementById("ICA").options[document.getElementById("ICA").selectedIndex].text;
+    var com = document.getElementById(component_type+"_select").value;
+    var ica = document.getElementById("ICA").value;
+    var ana = document.getElementById("Analysis").value;
+
+    var del = document.querySelectorAll('div[class^="c_"]')
+    for (var i = 0; i < del.length; i++) {
+      del[i].style.display = "none";
+    } 
+    var sel = document.getElementsByClassName("c_"+com+"_"+ica);  
+    for (var i = 0; i < sel.length; i++) {
+      sel[i].style.display = "inline";
+    
+    }
+
+    for (var i = 0;i<document.getElementById("Analysis").length; i++) {
+      var j = document.getElementById("Analysis")[i].value;
+      document.getElementById(ica+"_"+com+"_"+j).style.display = "none";
+
+    }
+
+    var new_img = ica+"_"+com+"_"+ana;
+      
+    document.getElementById(new_img).style.display="inline";
+
+    document.getElementById("image_link").innerHTML = document.getElementById(new_img).src;
+    document.getElementById("image_link").href = document.getElementById(new_img).src.substring(0,document.getElementById(new_img).src.lastIndexOf("/")+1);
+
+  }
+
+
+  window.addEventListener("error", function(e) {              // when an error happens
+    if(e.target.tagName.toLowerCase() === "img") {          // if the target is an image
+        e.target.src = "images/missing.png";                       // then change its src to whatever you want
+    }
+}, true);
+
+
+
+
+
