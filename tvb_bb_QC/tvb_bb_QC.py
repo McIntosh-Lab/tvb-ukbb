a387e598bae95a0f214570cf9ec6c51e4a3ce2ab
Noah Frazier-Logue <nfrazier-logue@research.baycrest.org>
2021-05-07 16:41:40 -0400

    all subpipes can now be run standalone; bugfixes for batch script

--- tvb_bb_QC/tvb_bb_QC.py
+++ tvb_bb_QC/tvb_bb_QC.py
@@ -22,8 +22,13 @@
 # limitations under the License.
 #
 
-import bb_pipeline_tools.bb_logging_tool as LT  
+
 import os.path
+import sys
+import json
+
+sys.path.insert(1, os.path.dirname(__file__) + "/..")
+import bb_pipeline_tools.bb_logging_tool as LT
 
 
 def tvb_bb_QC(subject, jobHold, fileConfiguration):
@@ -48,3 +53,24 @@ def tvb_bb_QC(subject, jobHold, fileConfiguration):
     )
     print("SUBMITTED QC")
     return jobQC
+
+
+if __name__ == "__main__":
+    # grab subject name from command
+    subject = sys.argv[1]
+    fd_fileName = "logs/file_descriptor.json"
+
+    # check if subject directory exists
+    if not os.path.isdir(subject):
+        print(f"{subject} is not a valid directory. Exiting")
+        sys.exit(1)
+    # attempt to open the JSON file
+    try:
+        json_path = os.path.abspath(f"./{subject}/{fd_fileName}")
+        with open(json_path, "r") as f:
+            fileConfig = json.load(f)
+    except:
+        print(f"{json_path} could not be loaded. Exiting")
+        sys.exit(1)
+    # call pipeline
+    tvb_bb_QC(subject, "-1", fileConfig)
