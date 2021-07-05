ee3ff204cd55810285b68032ca2fb53226de291c
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2021-07-05 01:36:24 -0400

    gentvb input compatibility. erating centres, cortical, hemispheres txts. renaming connectivity files and replacing nans with 0

---
+++ bb_diffusion_pipeline/bb_probtrackx2/generate_centres_cortical.py
@@ -0,0 +1,92 @@
+#!/bin/env python
+
+import nibabel as nib
+import numpy as np
+import sys
+import csv
+import os
+np.set_printoptions(threshold=sys.maxsize)
+
+
+def generate_centres_cortical(subjdir, PARC_LUT):
+	datafile = open(PARC_LUT, 'r')
+	datareader = csv.reader(datafile, delimiter = " ")
+	ROI_list = []
+
+	for row in datareader:
+		row[0]=int(row[0])
+		ROI_list.append(row)
+
+	ROI_list=sorted(ROI_list,key=lambda l:l[0])
+
+	label_image = os.path.join(subjdir,"T1/labelled_GM.nii.gz")
+	img = nib.load(label_image)
+	data = img.get_fdata()
+	#print(t1_data[np.nonzero(t1_data)])
+
+
+	centres_file=os.path.join(subjdir,"tvb_inputs/centres.txt")
+	cortical_file=os.path.join(subjdir,"tvb_inputs/cortical.txt")
+	hemi_file=os.path.join(subjdir,"tvb_inputs/hemisphere.txt")
+
+	f = open(centres_file, "a")
+	g = open(cortical_file, "a")
+	h = open(hemi_file, "a")
+
+	for row in ROI_list:
+		ROI_num = row[0]
+		ROI_name = row[1]
+		result = np.where(data == ROI_num)
+		x_cog=((np.mean(result[0])-(img.header['dim'][1]-1)/2)*img.header['pixdim'][0])
+		y_cog=((np.mean(result[1])-(img.header['dim'][2]-1)/2)*img.header['pixdim'][1])
+		z_cog=((np.mean(result[2])-(img.header['dim'][3]-1)/2)*img.header['pixdim'][2])
+		
+		#some of the ROIs in LUT arent in the SCFC... how do we know which ones will be used for the final weights matrices?
+		if np.isnan(x_cog) or np.isnan(y_cog) or np.isnan(z_cog):
+			print("found nan")
+		else:	
+
+			f.write(str(row[1])+"\t"+str(x_cog)+"\t"+str(y_cog)+"\t"+str(z_cog)+"\n")
+
+			#todo find pattern for subcort/cort
+			if ROI_num > 400:
+				g.write("0\n")
+			else:
+				g.write("1\n")
+
+
+			if "lh" in ROI_name or "LH" in ROI_name:
+				h.write("0\n")
+			else:
+				h.write("1\n")
+
+
+
+	f.close()
+	g.close()
+	h.close()
+
+
+if __name__ == "__main__":
+    """Function that generates SC, FC, TL, TS plots for QC html report
+    for a subject.
+
+    
+    Usage
+    ----------
+    python  SC_FC.py  subj
+    
+
+    Arguments
+    ----------
+    subj : 
+        Full path to subject's directory.
+
+    """
+    # try:
+    generate_centres_cortical(sys.argv[1],sys.argv[2])
+
+
+
+
+
