0eeb540365bf47422984c19f27d13c29fa82ae40
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2021-09-09 07:57:40 -0400

    removing TSNR IDP reference to baset/rfMRI. starting susceptibility SNR script

--- tvb_bb_QC/tvb_susceptibility_SNR_IDP_gen.sh
+++ tvb_bb_QC/tvb_susceptibility_SNR_IDP_gen.sh
@@ -2,30 +2,38 @@
 #
 # Script name: bb_IDP_func_TSNR
 #
-# Description: Script to generate the IDPs related to TSNR for fMRI.
-#
-# Authors: Fidel Alfaro-Almagro, Stephen M. Smith & Mark Jenkinson
-#
-# Copyright 2017 University of Oxford
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Description: Script to generate the IDPs related to TSNR for susceptible vs non-susceptible ROIs
+
+
+
 
 origDir=`pwd`
 scriptName=`basename "$0"`
 direc=$1
 
 cd $direc
+
+
+
+bin_suscept_parc_img=`basename $SUSCEPT_PARC_IMG`
+bin_suscept_parc_img="IDP_files/binmask_$bin_suscept_parc_img"
+fslmaths $SUSCEPT_PARC_IMG -bin $bin_suscept_parc_img
+
+bin_suscept_rois=`basename $SUSCEPT_ROIS`
+bin_suscept_rois="IDP_files/binmask_$bin_suscept_rois"
+fslmaths $SUSCEPT_ROIS -bin $bin_suscept_rois
+
+inv_bin_suscept_rois=`basename $SUSCEPT_ROIS`
+inv_bin_suscept_rois="IDP_files/inv_binmask_$bin_suscept_rois"
+fslmaths $bin_suscept_parc_img -sub $bin_suscept_rois $inv_bin_suscept_rois
+
+#TODO: register parcellations to T1 
+#${FSLDIR}/bin/applywarp --rel --interp=nn --in=$PARC_IMG --ref=T1 -w T1_to_MNI_warp_coef_inv -o parcel_to_T1
+
+#TODO: mask out fMRI files for r and t, using the below code to generate SNR values
+#TODO: call this script from tvb_bb_QC.sh
+#TODO: ensure this is IDP added to the tvb_ukbb IDPs as well as CAm-CAN IDPs and the scripts that generate IDP reports. 
+
 
 basefMRI="fMRI/"
 #if [ -d $basefMRI/unusable ] ; then
