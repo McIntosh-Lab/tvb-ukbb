4f59c4cab1e00f622b6926a9cc8e1e46b5529be4
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2022-03-09 14:48:58 -0500

    tvb_merge_bvecbval.py indentation fix

--- bb_pipeline_tools/tvb_merge_bvecbval.py
+++ bb_pipeline_tools/tvb_merge_bvecbval.py
@@ -11,19 +11,19 @@ import numpy as np
 import sys
 
 def merge_bvalbvec(bval_file_1 bval_file_2 bvec_file_1 bvec_file_2 bval_output_name bvec_output_name):
-bval1 = np.loadtxt(bval_file_1) 
-bval2 = np.loadtxt(bval_file_2) 
+    bval1 = np.loadtxt(bval_file_1) 
+    bval2 = np.loadtxt(bval_file_2) 
 
-bvec1 = np.loadtxt(bvec_file_1) 
-bvec2 = np.loadtxt(bvec_file_2) 
+    bvec1 = np.loadtxt(bvec_file_1) 
+    bvec2 = np.loadtxt(bvec_file_2) 
 
-bval = np.concatenate((bval1, bval2))
-bval = bval[:,np.newaxis]
-bval = bval.T
-np.savetxt(bval_output_name,bval,fmt='%i')
+    bval = np.concatenate((bval1, bval2))
+    bval = bval[:,np.newaxis]
+    bval = bval.T
+    np.savetxt(bval_output_name,bval,fmt='%i')
 
-bvec = np.concatenate((bvec1, bvec2), axis=1)
-np.savetxt(bvec_output_name,bvec,fmt='%0.6f')
+    bvec = np.concatenate((bvec1, bvec2), axis=1)
+    np.savetxt(bvec_output_name,bvec,fmt='%0.6f')
 
 
 if __name__ == "__main__":
