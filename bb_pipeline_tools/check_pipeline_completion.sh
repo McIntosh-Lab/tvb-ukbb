2e69d0ae38d0701faf9f3dbe92dba7679978e7ed
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2022-03-25 00:49:01 -0400

    output and rawdata checkers added to pipeline tools

---
+++ bb_pipeline_tools/check_pipeline_completion.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# 
+# this script should be run while in an ADNI3 directory (ls should show AD, CN, LMCI, etc directories)
+# This script checks processed subjects for completion - missing expected
+# output files will be printed if not found.
+#
+# Usage while in directory containing subjects: 
+#       check_pipeline_completion.sh  list_of_subjects.tsv
+#
+#   list_of_subjects.tsv should be a file containing list of subjects to be checked, one per line. subj name and group (AD, CN, etc) should be on each line, separated by a tab
+# 
+# Author: Justin Wang
+
+
+#iterate through subjlist
+while IFS=$' \t\r\n' read -r subjname group; do
+    if [ -d ${group}/${subjname} ]
+    then
+        cd ${group}/${subjname}
+
+        #array of ica dirs
+        array=()
+        while IFS=  read -r -d $'\0'; do
+            array+=("$REPLY")
+        done < <(find fMRI -maxdepth 1 -type d -name "*.ica" -print0)
+
+
+
+        #for each .ica file, check ts and fc
+        for t in ${array[@]}; do
+            rfMRI_ver=`basename $t`
+            if [ -f "fMRI/$rfMRI_ver/ts.txt" ]; then
+                :
+            else
+                echo "$subjname is missing ts.txt" |& tee -a ../feb10_raw_complete_check.txt
+            fi
+
+            if [ -f "fMRI/$rfMRI_ver/fc.txt" ]; then
+                :
+            else
+                echo "$subjname is missing fc.txt" |& tee -a ../feb10_raw_complete_check.txt
+            fi
+        done
+
+        #check sc.txt
+        if [ -f "dMRI/sc.txt" ]; then
+                :
+        else
+                echo "$subjname is missing sc.txt" |& tee -a ../feb10_raw_complete_check.txt
+        fi
+
+        #check QC HTML generation
+        if [ -f "QC/html/IDP.html" ]; then
+                :
+        else
+                echo "$subjname has incomplete QC report" |& tee -a ../feb10_raw_complete_check.txt
+        fi
+
+        #check IDP generation
+        if [ -f "IDP_files/significant_IDPs.tsv" ]; then
+                :
+        else
+                echo "$subjname has incomplete IDP processing" |& tee -a ../feb10_raw_complete_check.txt
+        fi
+
+        cd ../..
+    fi
+done < "$1"
