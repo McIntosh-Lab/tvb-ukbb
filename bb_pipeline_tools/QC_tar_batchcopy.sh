268bc000425833c96266429651b3000080e246dd
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2022-04-17 11:41:10 -0400

    batch copy scripts to Cam-CAN specific

--- bb_pipeline_tools/QC_tar_batchcopy.sh
+++ bb_pipeline_tools/QC_tar_batchcopy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# this script should be run while in an ADNI3 directory (ls should show AD, CN, LMCI, etc directories)
+# this script should be run while in an CC directory (ls should show subject directories)
 # this script will batch QC_tar your run subjects
 #
-# usage: QC_tar_batchcopy.sh ADNI_subject_list.tsv $tvb_dir
+# usage: QC_tar_batchcopy.sh CC_subject_list.tsv $tvb_dir
 
 tvb_dir="${2}"	
 mkdir -p QC_tar
-while IFS=$' \t\r\n' read -r subjname group; do
-	mkdir -p QC_tar/${group}
-	${tvb_dir}/bb_pipeline_tools/QC_tar.sh ${subjname} ${group}
-	mv ${group}/${subjname}_QC.tar QC_tar/${group}/
+while IFS=$' \t\r\n' read -r subjname; do
+	mkdir -p QC_tar
+	${tvb_dir}/bb_pipeline_tools/QC_tar.sh ${subjname}
+	mv ${subjname}_QC.tar QC_tar/
 done < "$1"
--- bb_pipeline_tools/subj_tar_batchcopy.sh
+++ bb_pipeline_tools/subj_tar_batchcopy.sh
@@ -5,7 +5,7 @@
 # Description: Script to batch generate subj tars for all processed subs and copy them into subj_tar
 #
 # Usage: 
-#         While in subjects folder (you should see AD, CN, EMCI etc subdirs):  subj_tar_batchcopy.sh subjects_tsv
+#         While in subjects folder (you should see subdirs):  subj_tar_batchcopy.sh subjects_tsv
 #
 ## Author: Justin Wang
 
@@ -18,10 +18,10 @@ oridDir=`pwd`
 mkdir subj_tar
 
 # while IFS= read -r subjname; do
-while IFS=$' \t\r\n' read -r subjname group; do
-  mkdir subj_tar/${group}
-  tar -czvf ${group}/${subjname}.tar.gz ${group}/${subjname}
-  mv ${group}/${subjname}.tar.gz subj_tar/${group}/
+while IFS=$' \t\r\n' read -r subjname; do
+  mkdir subj_tar
+  tar -czvf ${subjname}.tar.gz ${subjname}
+  mv ${subjname}.tar.gz subj_tar/
 
 done < "$1"
 
