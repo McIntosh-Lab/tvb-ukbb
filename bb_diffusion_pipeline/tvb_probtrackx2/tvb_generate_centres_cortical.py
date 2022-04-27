e4233fac4d5fa85f8e4f2cf69fcee65748348eb9
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2022-04-27 15:32:51 -0400

    cortical.txt tvb input generated based off capitalization of hemisphere from lut

--- bb_diffusion_pipeline/tvb_probtrackx2/tvb_generate_centres_cortical.py
+++ bb_diffusion_pipeline/tvb_probtrackx2/tvb_generate_centres_cortical.py
@@ -51,7 +51,8 @@ def generate_centres_cortical(subjdir, PARC_LUT, PARC_NAME):
 		f.write(str(row[1])+"\t"+str(x_cog)+"\t"+str(y_cog)+"\t"+str(z_cog)+"\n")
 
 		#todo find pattern for subcort/cort
-		if ROI_num > 400:
+		# if ROI_num > 400:
+		if "lh" in ROI_name or "rh" in ROI_name:
 			g.write("0\n")
 		else:
 			g.write("1\n")
