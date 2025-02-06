52ec8a5d9b7730fe636441885f2ddd1f995d2b7c
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2025-02-06 01:11:07 -0700

    fix(QC): typo bugfix for lightbox (not generating extraction images)

--- tvb_bb_QC/lightbox.sh
+++ tvb_bb_QC/lightbox.sh
@@ -200,7 +200,7 @@ subject_folder=$2
 		fi
 	fi
         
-        if [ "$label_underlay_flag" -q "0" ]; then
+        if [ "$label_underlay_flag" -eq "0" ]; then
 		for ((i=0; i<3; i++)); do
 
 			${FSLDIR}/bin/fsleyes render --outfile $output_folder/$sub"_$3_"${axisNames[$i]}.png  --crop 30 --size 6400 2400 --scene lightbox --displaySpace $subject_folder$underlay --zaxis $i --sliceSpacing ${sliceSpacing[$i]} --zrange ${zrange[2*$i]} ${zrange[2*$i+1]} --ncols 6 --nrows 3 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --cursorColour 0.0 1.0 0.0 --colourBarLocation top --colourBarLabelSide top-left --colourBarSize 100.0 --labelSize 12 $subject_folder$underlay --name "underlay" --overlayType volume --alpha 100.0 --brightness $brightness_var --contrast $contrast_var --cmap greyscale --negativeCmap greyscale --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 $overlay_args
